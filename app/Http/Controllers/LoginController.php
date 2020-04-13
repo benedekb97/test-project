b92cff653cd1f400d384c4f0aa15d786cb08dd98
Benedek Burgess <benedekb97@gmail.com>
2020-04-13 11:49:19 +0200

    Add login script

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $user = User::all()->where('username', $username)->first();
+
+        if($user && $user->login_attempts>2) {
+            $rules = ['captcha' => 'required|captcha'];
+            $validator = validator()->make(request()->all(), $rules);
+            if($validator->fails()) {
+                return view('login', [
+                    'error' => 'Bejelentkezés sikertelen! Hibás Captcha',
+                    'captcha' => true
+                ]);
+            }
+        }
+
+        if(Auth::attempt([
+            'username' => $username,
+            'password' => $password
+        ])) {
+            $user->login_attempts = 0;
+            $user->save();
+            return redirect()->route('index');
+        } else {
+
+            if($user) {
+                $user->login_attempts++;
+                $user->save();
+
+                if($user->login_attempts>2) {
+                    $captcha = true;
+                }else{
+                    $captcha = false;
+                }
+            }
+
+            return view('login', [
+                'error' => 'Bejelentkezés sikertelen!',
+                'captcha' => $captcha
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        return redirect()->route('index');
+    }
+}
