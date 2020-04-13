5cdb9001a17c998b574506ec0539c3b17bf98213
Benedek Burgess <benedekb97@gmail.com>
2020-04-13 13:48:38 +0200

    Cleanup
    Removed unused parameters from methods and unused class from User model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,31 +7,31 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         if(Auth::check()) {
             return redirect()->route('user');
-        }else{
-            return view('login');
         }
+
+        return view('login');
     }
 
-    public function user(Request $request)
+    public function user()
     {
         return view('user');
     }
 
-    public function editor(Request $request)
+    public function editor()
     {
         return view('editor');
     }
 
-    public function admin(Request $request)
+    public function admin()
     {
         $groups = Auth::user()->groups;
-        $text = "";
+        $text = '';
         foreach($groups as $group) {
-            $text .= $group->name . ", ";
+            $text .= $group->name . ', ';
         }
 
         $text = substr($text, 0, -2);
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -31,32 +31,31 @@ class LoginController extends Controller
             'password' => $password
         ])) {
             $user->login_attempts = 0;
-            $user->last_login = date("Y-m-d H:i:s");
+            $user->last_login = date('Y-m-d H:i:s');
             $user->save();
             return redirect()->route('admin');
-        } else {
+        }
 
-            if($user) {
-                $user->login_attempts++;
-                $user->save();
+        if($user) {
+            $user->login_attempts++;
+            $user->save();
 
-                if($user->login_attempts>2) {
-                    $captcha = true;
-                }else{
-                    $captcha = false;
-                }
+            if($user->login_attempts>2) {
+                $captcha = true;
             }else{
                 $captcha = false;
             }
-
-            return view('login', [
-                'error' => 'Bejelentkezés sikertelen!',
-                'captcha' => $captcha
-            ]);
+        }else{
+            $captcha = false;
         }
+
+        return view('login', [
+            'error' => 'Bejelentkezés sikertelen!',
+            'captcha' => $captcha
+        ]);
     }
 
-    public function logout(Request $request)
+    public function logout()
     {
         Auth::logout();
 
